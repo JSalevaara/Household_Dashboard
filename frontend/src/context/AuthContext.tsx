60df3036d85c4fd875735935f82f097820f49d44
Juho S <juho@salevaara.fi>
2026-06-24 22:39:12 +0300

    Removed /api from some routes.

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -32,7 +32,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 		}
 
 		try {
-			const response = await apiClient.get('/api/users/me');
+			const response = await apiClient.get('/users/me');
 			setCurrentUser(response.data);
 		} catch (error) {
 			console.error('Token invalid or expired');
